1d0ac1b4ae1e5db61769bd9e82d2f810dec9783d
atanda0x <atandanafiu21@gmail.com>
2023-08-30 06:35:24 -0700

    create a user controller file

---
+++ controllers/userController.go
@@ -0,0 +1,35 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetUsers() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func GetUser() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func Signup() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func Login() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func HashPassword(password string) string {
+
+}
+
+func VerifyPassword(userPassword string, providePassword string) (bool, string) {
+
+}
